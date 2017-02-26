3750781753d0703a767ce9de27327b2d14f2ef16
coursework <coursework@coursework.csc.tntech.edu>
2017-02-26 15:29:44 -0600

    added many new files and switched from gas-mode to asm86-mode for assembly

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(message-suppression-these
+   (quote
+    ("Error during redisplay: (jit-lock-function 1) signaled (void-variable gas-hi-global-undef)" "Error running timer `gas-symbol-highlight-maybe': (void-variable gas-changed)")))
  '(package-selected-packages
    (quote
     (xah-find zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window))))
---
+++ personal/preload/message-suppression.el
@@ -0,0 +1,66 @@
+;; message-suppression.el
+;; a quick hack by Aaron (me@aaron-miller.me), 2013-11-12
+;; half a solution for http://superuser.com/questions/669701/emacs-disable-some-minibuffer-messages
+;; NB this does nothing until you 
+;; M-x customize-group RET message-suppression RET
+;; and adjust to taste
+
+(defgroup message-suppression nil
+  "Customization options for selective message suppression."
+  :prefix "message-suppression")
+
+(defcustom message-suppression-enabled nil
+  "Whether or not to suppress messages listed in
+`message-suppress-these'."
+  :group 'message-suppression
+  :tag "Suppress some messages?"
+  :type '(choice (const :tag "No" nil)
+                 (const :tag "Yes" t)))
+
+(defcustom message-suppression-to-messages-buffer t
+  "Whether or not to insert messages suppressed from the
+minibuffer into the *Messages* buffer."
+  :group 'message-suppression
+  :tag "Insert suppressed messages into *Messages* buffer?"
+  :type '(choice (const :tag "No" t)
+                 (const :tag "Yes" nil)))
+
+(defcustom message-suppression-these nil
+  "A list of messages which the `message-except-these' advice
+should suppress from being echoed in the minibuffer. Messages
+are matched by `member', i.e., only exact strings match.
+
+NB! Per the Emacs manual, calls from C code to primitives (such
+as `message') ignore advice entirely, which means some messages
+cannot be suppressed by this mechanism. ('Advising
+Functions' in the Emacs Lisp Reference Manual, q.v.)"
+  :group 'message-suppression
+  :tag "Messages to suppress"
+  :type '(repeat (string))
+  :link '(info-link "(elisp)Advising Functions"))
+
+(defadvice message (around message-suppress-advice)
+  "Suppress messages listed in `message-suppress-these' from being
+  echoed in the minibuffer."
+  (let ((message-string nil)
+        (current-buffer nil))
+    (if (and message-suppression-enabled
+             (length (ad-get-args 0))
+             (stringp (car (ad-get-args 0)))
+             ;; message-string doesn't get set until here because `format'
+             ;; will complain if its first argument isn't a string
+             (setq message-string (apply 'format (ad-get-args 0)))
+             (member message-string
+                     message-suppression-these))
+        ;; we won't call `message', but we might echo to *Messages*
+        (and message-suppression-to-messages-buffer
+             (progn
+               (setq current-buffer (current-buffer))
+               (switch-to-buffer (get-buffer-create "*Messages*"))
+               (goto-char (point-max))
+               (insert (make-string 1 10))
+               (insert message-string)
+               (switch-to-buffer current-buffer)))
+      ad-do-it)))
+
+(ad-activate 'message)
